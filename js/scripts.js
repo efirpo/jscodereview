cdc9db0dc0916723abe36aeb1b3c61c7d068a219
Ethan Firpo <efirpo@gmail.com>
2020-03-27 10:09:07 -0700

    add question content, finalize styles, add initial javascript

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// value 1 = C#, value 2 = Python, value 3 = Ruby
+
+$(document).ready(function() {
+
+  $("#progSurvey").submit(function(event){
+
+    event.preventDefault();
+    
+
+  });
+
+});
