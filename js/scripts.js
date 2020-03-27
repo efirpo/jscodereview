02127bfc3bf4263d4297ce3fda65de4b2461e492
Ethan Firpo <efirpo@gmail.com>
2020-03-27 15:51:13 -0700

    change site to hide survey after completion, edit layout of page

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ $(document).ready(function() {
     console.log(inputGoal, inputBrackets, inputReptile, inputThink, inputJoke);
     var inputResult = (inputGoal  + inputBrackets + inputReptile)
 
+    $("#progSurvey").hide();
+
     if (jQuery.inArray (inputResult, ["111", "112", "121", "211", "113", "131", "311"]) >=0) {
       $(".cSharp").fadeIn();
       $(".ruby").hide();
@@ -41,15 +43,15 @@ $(document).ready(function() {
       $(".ruby").hide();
       $(".python").hide();
       $(".sorry").fadeIn();
-      $("#joke1").hide();
-      $("#joke2").hide();
-      $("#joke3").hide();
+      $(".joke1").hide();
+      $(".joke2").hide();
+      $(".joke3").hide();
     }
 
     if (inputJoke == "blank") {
-      $("#joke1").hide();
-      $("#joke2").hide();
-      $("#joke3").hide();
+      $(".joke1").hide();
+      $(".joke2").hide();
+      $(".joke3").hide();
       $(".cSharp").hide();
       $(".ruby").hide();
       $(".python").hide();
@@ -57,27 +59,27 @@ $(document).ready(function() {
     }
 
     else if (inputJoke == "1") {
-      $("#joke1").fadeIn();
-      $("#joke2").hide();
-      $("#joke3").hide();
+      $(".joke1").fadeIn();
+      $(".joke2").hide();
+      $(".joke3").hide();
     }
 
     else if (inputJoke == "2") {
-      $("#joke1").hide();
-      $("#joke2").fadeIn();
-      $("#joke3").hide();
+      $(".joke1").hide();
+      $(".joke2").fadeIn();
+      $(".joke3").hide();
     }
 
     else if (inputJoke == "3") {
-      $("#joke1").hide();
-      $("#joke2").hide();
-      $("#joke3").fadeIn();
+      $(".joke1").hide();
+      $(".joke2").hide();
+      $(".joke3").fadeIn();
     }
 
     else {
-      $("#joke1").hide();
-      $("#joke2").hide();
-      $("#joke3").hide();
+      $(".joke1").hide();
+      $(".joke2").hide();
+      $(".joke3").hide();
     }
 
     if (inputThink == "blank") {
@@ -85,36 +87,36 @@ $(document).ready(function() {
       $(".ruby").hide();
       $(".python").hide();
       $(".sorry").fadeIn();
-      $("#joke1").hide();
-      $("#joke2").hide();
-      $("#joke3").hide();
+      $(".joke1").hide();
+      $(".joke2").hide();
+      $(".joke3").hide();
     }
     
     else if (inputThink == 1) {
-     $("div#joke1").removeClass();
-     $("div#joke1").addClass("jokeyellow");
-     $("div#joke2").removeClass();
-     $("div#joke2").addClass("jokeyellow");
-     $("div#joke3").removeClass();
-     $("div#joke3").addClass("jokeyellow");
+      $("div.joke1").removeClass();
+      $("div.joke1").addClass("jokeyellow");
+      $("div.joke2").removeClass();
+      $("div.joke2").addClass("jokeyellow");
+      $("div.joke3").removeClass();
+      $("div.joke3").addClass("jokeyellow");
     }
 
     else if (inputThink == 2) {
-      $("div#joke1").removeClass();
-      $("div#joke1").addClass("jokeblue");
-      $("div#joke2").removeClass();
-      $("div#joke2").addClass("jokeblue");
-      $("div#joke3").removeClass();
-     $("div#joke3").addClass("jokeblue");
+      $("div.joke1").removeClass();
+      $("div.joke1").addClass("jokeblue");
+      $("div.joke2").removeClass();
+      $("div.joke2").addClass("jokeblue");
+      $("div.joke3").removeClass();
+      $("div.joke3").addClass("jokeblue");
     }
 
     else if (inputThink == 3) {
-      $("div#joke1").removeClass();
-      $("div#joke1").addClass("jokeorange");
-      $("div#joke2").removeClass();
-      $("div#joke2").addClass("jokeorange");
-      $("div#joke3").removeClass();
-      $("div#joke3").addClass("jokeorange");
+      $("div.joke1").removeClass();
+      $("div.joke1").addClass("jokeorange");
+      $("div.joke2").removeClass();
+      $("div.joke2").addClass("jokeorange");
+      $("div.joke3").removeClass();
+      $("div.joke3").addClass("jokeorange");
     }
 
   });
