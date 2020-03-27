399710a010a520d92aff8fc3b2a58d4262b6f059
Ethan Firpo <efirpo@gmail.com>
2020-03-27 11:42:42 -0700

    add initial if/else functionality to javascript, edit styles

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,27 @@ $(document).ready(function() {
     var inputThink = $("#thinkResult").val();
     var inputJoke = $("#jokeResult").val();
     
-    console.log(inputGoal, inputBrackets, inputReptile, inputThink, inputJoke)
+    console.log(inputGoal, inputBrackets, inputReptile, inputThink, inputJoke);
 
-    
+    if (inputGoal == 1 && inputBrackets == 1 || inputBrackets == 1 && inputReptile == 1 || inputGoal == 1 && inputReptile == 1) {
+      $(".cSharp").show();
+      $(".ruby").hide();
+      $(".python").hide();
+    }
 
+    else if (inputGoal == 2 && inputBrackets == 2 || inputBrackets == 2 && inputReptile == 2 || inputGoal == 2 && inputReptile == 2) {
+      $(".cSharp").hide();
+      $(".ruby").show();
+      $(".python").hide();
+    }
+
+    else if (inputGoal == 3 && inputBrackets == 3 || inputBrackets == 3 && inputReptile == 3 || inputGoal == 3 && inputReptile == 3) {
+      $(".csharp").hide();
+      $(".ruby").hide();
+      $(".python").show();
+    }
+
+    else {alert("NOPE")}
   });
 
 });
